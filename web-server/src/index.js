603e40ebad732a6e78483158fb50daf9f4a170e9
Shafiq Ur Rehman <69121735+FsCode-shafiq@users.noreply.github.com>
2022-02-14 13:19:17 +0500

    add partials

--- web-server/src/index.js
+++ web-server/src/index.js
@@ -1,29 +1,42 @@
 const path = require('path');
 const express = require('express');
 const request = require('request');
+const hbs = require('hbs');
 const { getGeoLocation } = require('../utils/index');
 
 const app = express();
-const publicDirectoryPath = path.join(__dirname, '../public');
 
+//Define path for express config
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialPath = path.join(__dirname,'../templates/partials');
+
+// setup handlebars and views for express
 app.set('view engine', 'hbs')
+app.set('views', viewsPath);
+hbs.registerPartials(partialPath);
+
+//setup static directory to serve
 app.use(express.static(publicDirectoryPath));
 
-app.get('/home', (req, res) => {
+app.get('', (req, res) => {
     res.render('index', {
-        h1: 'Weather App'
+        h1: 'Weather App',
+        name: 'fsCode'
     });
 });
 
 app.get('/about', (req,res)=>{
     res.render('about', {
-        h1:'About page'
+        h1:'About page',
+        name: 'fsCode'
     })
 });
 
 app.get('/help', (req,res)=>{
     res.render('help',{
-        h1:'help page'
+        h1:'help page',
+        name: 'fsCode'
     })
 });
 
